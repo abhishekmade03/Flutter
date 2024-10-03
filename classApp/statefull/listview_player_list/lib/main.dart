13a2036cc7a5c2b836d421f80d26ddb7f3814848
abhishekmade03 <abhishekmade123@gmail.com>
2024-10-04 04:37:23 +0530

    1

---
+++ classApp/statefull/listview_player_list/lib/main.dart
@@ -0,0 +1,111 @@
+import "package:flutter/Material.dart";
+import "package:flutter/material.dart";
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatefulWidget {
+  const MainApp({super.key});
+  @override
+  State<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+  TextEditingController nameController = TextEditingController();
+  String? myName;
+  List<String> playerList = [];
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text(
+            "TextField ListView Demo",
+            style: TextStyle(fontSize: 30, fontWeight: FontWeight.w500),
+          ),
+          backgroundColor: Colors.orange,
+          centerTitle: true,
+        ),
+        body: Column(
+          children: [
+            const SizedBox(
+              height: 20,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: TextField(
+                controller: nameController,
+                style: const TextStyle(
+                  fontSize: 30,
+                ),
+                decoration: const InputDecoration(
+                  hintText: "Enter Name",
+                  hintStyle: TextStyle(
+                    fontSize: 25,
+                    color: Colors.grey,
+                  ),
+                  border: OutlineInputBorder(),
+                ),
+                onChanged: (String val) {
+                  print("value: $val");
+                },
+                onEditingComplete: () {
+                  print("Editing Complete");
+                },
+                onSubmitted: (value) {
+                  print("value Submitted: $value");
+                },
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            GestureDetector(
+              onTap: () {
+                print("Add Data");
+                myName = nameController.text.trim();
+                print("My Name:$myName");
+                if (myName != "") {
+                  playerList.add(myName!);
+                  print("PleysList length: ${playerList.length}");
+                  nameController.clear();
+                  setState(() {});
+                }
+              },
+              child: Container(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 15,
+                  vertical: 5,
+                ),
+                decoration: BoxDecoration(
+                  color: Colors.blue,
+                  borderRadius: BorderRadius.circular(15),
+                ),
+                child: const Text("Add Data",
+                    style: TextStyle(
+                      fontSize: 25,
+                      color: Colors.white,
+                    )),
+              ),
+            ),
+            const SizedBox(
+              height: 40,
+            ),
+            ListView.builder(
+                itemCount: playerList.length,
+                shrinkWrap: true,
+                itemBuilder: (context, index) {
+                  return Text(
+                    "Name:${playerList[index]}",
+                    style: const TextStyle(
+                      fontSize: 25,
+                    ),
+                  );
+                })
+          ],
+        ),
+      ),
+    );
+  }
+}
